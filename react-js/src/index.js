bcb2342c8999bb0b20830bfa4a02e1427d76849a
Barbara Kaminska <baskak98@gmail.com>
2020-12-12 11:08:03 +0100

    hackaton_start

--- react-js/src/index.js
+++ react-js/src/index.js
@@ -1,18 +1,15 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Card from './components/Card';
-//import CardClass from './CardClass';
-
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Card />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('my-app')
 );
 
 // If you want to start measuring performance in your app, pass a function
